17d77826cac74674e07a3ffb8c4d6f2893c6276a
user15213 <resomakar@gmail.com>
2025-02-13 17:55:19 +0500

    update text INDEX.js

--- INDEX.js
+++ INDEX.js
@@ -1,32 +1,3 @@
-// Пример встроенной функции
-Math.round(5.34); // 5
-
-// Встроенный в Node.js модуль для работы с файлами
-import fs from "fs";
-
-// Читает содержимое файла
-const data = fs.readFileSync("path/to/file");
-
-// По общепринятому соглашению, lodash импортируется под именем _
-import _ from "lodash";
-
-// Пересечение массивов
-_.intersection([2, 1], [2, 3]); // => [2]
-
-// capitalize делает первую букву заглавной
-_.capitalize("hello"); // Hello
-
-// Так будет происходить поиск файла lodash.js в текущей директории
-import _ from "./lodash.js";
-
-// Так импортируется код из пакета
-import _ from "lodash";
-
-"dependencies": {
-  "lodash": "^4.17.15"
-}
-
-import React from "react";
+const _ = require('lodash');
 
 console.log(_.last(['one', 'two']));
-
