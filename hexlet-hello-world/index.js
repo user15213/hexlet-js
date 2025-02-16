ab5305e7d52a095663b931f0063aacd3ddafe50d
user15213 <resomakar@gmail.com>
2025-02-16 17:20:55 +0500

    added simple code in index.js

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,1 +1,13 @@
-console.log('Hello, Hexlet!');
+console.log('Hello, Hexlet!')
+const sum = (a, b) => {
+  let result = a + b
+  return result
+}
+
+const multiply = (a, b) => {
+  if (a === 0) return a * b
+  else return a * b
+}
+
+console.log(sum(5, 10))
+console.log(multiply(0, 5))
